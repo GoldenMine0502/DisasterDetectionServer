4ffb312e7d105ba401f944e5c3465b5682b33aee
GoldenMine0502 <ehe123@naver.com>
2024-10-03 16:19:36 +0900

    refactor

--- disaster_detection/inference/views.py
+++ disaster_detection/inference/views.py
@@ -6,7 +6,6 @@ from disaster_detection.core.resnest_inference import inference
 
 # git subtree pull --prefix=disaster_detection/core https://github.com/GoldenMine0502/DiasterDetectionCore.git master --squash
 def inference_request(request):
-    result = inference(request.number)
     return JsonResponse({
-        'result': result
+        'result': inference(request.number)
     })
