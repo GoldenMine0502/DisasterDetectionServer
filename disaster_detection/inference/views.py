e92eb8b78fb013865816bb0657947944a140959e
GoldenMine0502 <ehe123@naver.com>
2024-10-03 16:46:37 +0900

    get image and inference

--- disaster_detection/inference/views.py
+++ disaster_detection/inference/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from PIL import Image
 
+from disaster_detection.core.resnest import convert_tensor
 from disaster_detection.core.resnest_inference import inference
 
 
 # git subtree pull --prefix=disaster_detection/core https://github.com/GoldenMine0502/DiasterDetectionCore.git master --squash
 def inference_request(request):
+    # POST일 때 입력받은 이미지를 통해 inference 결과를 보여줌
+    if request.method == 'POST' and request.FILES.get('image'):
+        image_file = request.FILES['image']
+        image = Image.open(image_file)
+        image = convert_tensor(image)
+
+        result = inference(image)
+        return JsonResponse({
+            'result': result
+        })
+
     return JsonResponse({
-        'result': inference(request.number)
+        'error': 'only supports post request'
     })
