bf6add9de43f753213fdeda56d22908c0e8ee9de
GoldenMine0502 <ehe123@naver.com>
2024-10-11 11:12:17 +0900

    test 완료

--- disaster_detection/inference/views.py
+++ disaster_detection/inference/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from PIL import Image
@@ -6,6 +7,12 @@ from core.vision_transformer_inference import inference
 from django.views.decorators.csrf import csrf_exempt
 
 
+def softmax(logits):
+    exp_values = np.exp(logits - np.max(logits))  # Overflow 방지를 위해 최댓값을 빼줌
+    return exp_values / np.sum(exp_values)
+
+
+# ['collapsed_building', 'fire', 'flooded_areas', 'normal', 'traffic_incident']
 # git subtree pull --prefix=disaster_detection/core https://github.com/GoldenMine0502/DiasterDetectionCore.git master --squash
 @csrf_exempt
 def inference_request(request):
@@ -14,7 +21,11 @@ def inference_request(request):
         image_file = request.FILES['image']
         image = Image.open(image_file)
         output, label = inference(image)
-        output = output.item()
+        # print(output, label)
+        # output = output.item()
+        output = softmax(output)
+        output = output.tolist()
+        output = [round(num * 100, 2) for num in output]
         label = label.item()
 
         return JsonResponse({
